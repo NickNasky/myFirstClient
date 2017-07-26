1766983f134a07671bdc8d75604a4e6bf1a219eb
Nick Nasky <nick.nasky@gmail.com>
2017-07-26 13:54:18 -0600

    😵 added modal to post new assignments

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ $.get('http://localhost:8080/')
   .then((data) => {
     console.log(data);
     for (let i = 0; i < data.length; i++){
-    $(".assignments").append(
+    $('.assignments').append(
         `<ul>
           <h1>${data[i].name}</h1>
           <li>${data[i].due_date}</li>
+          <li>Priority: ${data[i].priority}</li>
           <li>${data[i].description}</li>
-          <li>${data[i].priority}</li>
-          <li>${data[i].subject}</li>
+          <li>Subject: ${data[i].subject}</li>
         </ul>`
     )};
   });
+  $('.post-btn').click(postObj);
 });
+
+function postObj() {
+  let post = {};
+  post.name = $('#assignment').val()
+  post.due_date = $('#date').val();
+  post.priority = $('#priority').val();
+  post.description = $('#description').val();
+  post.subject = $('#subject').val();
+  $.post('http://localhost:8080/', post);
+  window.location.reload();
+}
