175c519c919613becd95cb6caad0e19bdef87221
Nick Nasky <nick.nasky@gmail.com>
2017-07-28 14:27:41 -0600

    💩💩💩💩💩💩💩💩💩💩

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let savedData = {};
 let host = 'https://sheltered-temple-20365.herokuapp.com/'
+// let host = 'http://localhost:8080'
 $(document).ready(() => {
 $.get(host)
   .then((data) => {
@@ -41,7 +42,7 @@ function getObj() {
         `<div class="underline">
         <ul>
           <h1>${data.name}</h1>
-          <li>ID: ${data.id}
+          <li>ID: ${data.id}<li>
           <li>${data.due_date}</li>
           <li>Priority: ${data.priority}</li>
           <li>${data.description}</li>
@@ -56,7 +57,7 @@ function postObj() {
   let post = {};
   post.name = $('#assignment').val()
   post.due_date = $('#date').val();
-  post.priority = $('#priority').val();
+  post.priority = parseInt($('#priority').val());
   post.description = $('#description').val();
   post.subject = $('#subject').val();
     if (post.name && post.due_date && post.priority && post.description && post.subject) {
@@ -69,35 +70,40 @@ function postObj() {
       });
     } else {
       event.preventDefault();
-      alert('Please fill out all fields!')
+      alert('Please fill out all fields correctly!')
     }
 }
 
 function putObj() {
   let put = {};
-  idNum = $('#assignmentId').val();
+
+  let idNum = $('#assignmentId').val();
   put.name = $('#assignment1').val();
   put.due_date = $('#date1').val();
-  put.priority = $('#priority1').val();
-  put.description =$('#description1').val();
+  put.priority = parseInt($('#priority1').val());
+  put.description = $('#description1').val();
   put.subject = $('#subject1').val();
-  if(idNum && put.name && put.due_date && put.priority && put.description && put.subject) {
-    $.ajax({
-      url: host + idNum,
-      method: 'PUT',
-      data: put
-    })
-    .then(data => {
+  console.log(put);
+  console.log(idNum);
+  if (idNum && put.name && put.due_date && put.priority && put.description && put.subject) {
+    let settings = {
+    url: host + '/' + idNum,
+    method: 'PUT',
+    data: put
+    }
+    console.log(settings);
+    $.ajax(settings)
+    .then((data) => {
       window.location.reload();
-    })
-    .catch((data) => {
-    alert (data.responseJSON.Error)
-    });
+      })
+        .catch((data) => {
+          alert('Enter correct data')
+        });
   } else {
     event.preventDefault();
     alert('Please fill out all fields correctly!');
   };
-}
+};
 
 function delObj() {
   let idNum = $(this).attr('id');
